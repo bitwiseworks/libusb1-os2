0f560eb89092f5529802a25d9ba7e52bb2ac191a
SilvanScherrer <silvan.scherrer@aroa.ch>
2022-03-16 19:26:23 +0100

    cherry pick latest changes from libusb vendor

--- libusb/core.c
+++ libusb/core.c
@@ -2346,13 +2346,14 @@ int API_EXPORTED libusb_init(libusb_context **ctx)
 	list_add(&_ctx->list, &active_contexts_list);
 	usbi_mutex_static_unlock(&active_contexts_lock);
 
-	usbi_hotplug_init(_ctx);
-
 	if (usbi_backend.init) {
 		r = usbi_backend.init(_ctx);
 		if (r)
 			goto err_io_exit;
 	}
+
+	/* Initialize hotplug after the initial enumeration is done. */
+	usbi_hotplug_init(_ctx);
 
 	if (ctx)
 		*ctx = _ctx;
@@ -2441,6 +2442,7 @@ void API_EXPORTED libusb_exit(libusb_context *ctx)
 	for_each_device(_ctx, dev) {
 		usbi_warn(_ctx, "device %d.%d still referenced",
 			dev->bus_number, dev->device_address);
+		DEVICE_CTX(dev) = NULL;
 	}
 
 	if (!list_empty(&_ctx->open_devs))
